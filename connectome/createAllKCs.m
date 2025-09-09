a0430463c871daa5423f0f3ef4cdee35eb8a70de
PhiFischer <phi.fischer1@gmail.com>
2025-09-09 12:12:26 +0100

    removed legacy files from original model (CompensatoryVariability fork)

--- connectome/createAllKCs.m
+++
@@ -1,6 +0,0 @@
-for i=0:1926
-    kc = KCskel.createKC(i);
-    kc = kc.getDistBetweenPNInputs;
-    kc = kc.getDistPostPedToPNInputs;
-    kc.saveKCskel;
-end
--- connectome/saveKCfeaturesToCSV.m
+++
@@ -1,7 +0,0 @@
-paramNames = {'numPNsPerKC', 'numPNKCsynapsesPerKC', ...
-    'meanWPerKC', 'PNDistToPedPerKC', 'numAPLKCsynapsesPerKC'};
-table = array2table(allParams,'VariableNames', paramNames);
-table.KCindex = allKCindices;
-table.bodyId = KCskel.getBodyIDs(allKCindices);
-table.subtype = KCskel.getTypes(allKCindices);
-writetable(table,'Fig7_connectome_data.csv');
